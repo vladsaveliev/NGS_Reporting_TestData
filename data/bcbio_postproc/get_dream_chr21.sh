89e87c8f48016e73c6bd6d9b260182e1efea2bb9
vladsaveliev <vladsaveliev@me.com>
2017-06-23 01:28:29 +0300

    Add actual bam files to data

--- data/bcbio_postproc/get_dream_chr21.sh
+++ data/bcbio_postproc/get_dream_chr21.sh
@@ -3,8 +3,8 @@ mv $BASEDIR $BASEDIR.bak
 rsync -tavz --exclude "*.bam" --exclude "work" --exclude "input" --exclude "final_*" --exclude "final-*" chi:/Sid/vsaveliev/dream-chr21/ $BASEDIR
 rm $BASEDIR/config/run_info_ExomeSeq.yaml
 sed -i '' s%/Sid/vsaveliev/dream-chr21/input/%../input/%g $BASEDIR/config/dream-chr21.yaml
-ln -s ../../../bams/syn3-normal-ready.bam $BASEDIR/final/syn3-normal/
-ln -s ../../../bams/syn3-tumor-ready.bam $BASEDIR/final/syn3-tumor/
+cp bams/syn3-normal-ready.bam $BASEDIR/final/syn3-normal/
+cp bams/syn3-tumor-ready.bam $BASEDIR/final/syn3-tumor/
 rm $BASEDIR/final/syn3-normal/syn3-normal-ready.bam.bai
 rm $BASEDIR/final/syn3-tumor/syn3-tumor-ready.bam.bai
 mkdir $BASEDIR/input
