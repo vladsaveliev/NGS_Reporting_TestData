f29c07ed01e2bc895d69614b4912a41713769ec2
vladsaveliev <vladsaveliev@me.com>
2017-06-22 22:09:33 +0300

    Update test data to bcbio 1.0.4a with cnvkit, viral, and vcfanno

--- data/bcbio_postproc/get_dream_chr21.sh
+++ data/bcbio_postproc/get_dream_chr21.sh
@@ -1,16 +1,14 @@
 BASEDIR=dream_chr21
 mv $BASEDIR $BASEDIR.bak
-rsync -tavz --exclude "*.bam" --exclude "work" chi:/Sid/vsaveliev/dream-chr21/ $BASEDIR
-cd $BASEDIR
-sed -i '' s%/Sid/vsaveliev/dream-chr21/input/%../input/%g config/dream-chr21.yaml
-rm config/run_info_ExomeSeq.yaml
-cp ~/googledrive/az/analysis/dream_chr21/bams/syn3-normal-ready.key_genes_cds.bam final/syn3-normal/syn3-normal-ready.bam
-cp ~/googledrive/az/analysis/dream_chr21/bams/syn3-tumor-ready.key_genes_cds.bam final/syn3-tumor/syn3-tumor-ready.bam
-rm final/syn3-normal/syn3-normal-ready.bam.bai
-rm final/syn3-tumor/syn3-tumor-ready.bam.bai
-rm input
-mkdir input
-cp ~/googledrive/az/analysis/dream_chr21/input/NGv3.chr21.4col.bed input
+rsync -tavz --exclude "*.bam" --exclude "work" --exclude "input" --exclude "final_*" --exclude "final-*" chi:/Sid/vsaveliev/dream-chr21/ $BASEDIR
+rm $BASEDIR/config/run_info_ExomeSeq.yaml
+sed -i '' s%/Sid/vsaveliev/dream-chr21/input/%../input/%g $BASEDIR/config/dream-chr21.yaml
+ln -s ../../../bams/syn3-normal-ready.bam $BASEDIR/final/syn3-normal/
+ln -s ../../../bams/syn3-tumor-ready.bam $BASEDIR/final/syn3-tumor/
+rm $BASEDIR/final/syn3-normal/syn3-normal-ready.bam.bai
+rm $BASEDIR/final/syn3-tumor/syn3-tumor-ready.bam.bai
+mkdir $BASEDIR/input
+cp NGv3.chr21.4col.bed $BASEDIR/input
 
 echo ""
 echo "To compare:"
