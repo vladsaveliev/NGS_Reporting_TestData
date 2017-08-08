3684b54da16dc5714c60cce4ca441f3ca8ec97f5
vladsaveliev <vladsaveliev@me.com>
2017-08-08 13:00:31 +0300

    Pull test data from custom chihua location

--- data/bcbio_postproc/get_dream_chr21.sh
+++ data/bcbio_postproc/get_dream_chr21.sh
@@ -1,12 +1,14 @@
-BASEDIR=dream_chr21
+BASEDIR=$1
 mv $BASEDIR $BASEDIR.bak
-rsync -tavz --exclude "*.bam" --exclude "work" --exclude "input" --exclude "final_*" --exclude "final-*" chi:/Sid/vsaveliev/dream-chr21/ $BASEDIR
+rsync -tavz --exclude "*.bam" --exclude "work" --exclude "input" --exclude "final_*" --exclude "final-*" chi:/Sid/vsaveliev/$BASEDIR/ $BASEDIR
 rm $BASEDIR/config/run_info_ExomeSeq.yaml
-sed -i '' s%/Sid/vsaveliev/dream-chr21/input/%../input/%g $BASEDIR/config/dream-chr21.yaml
-cp bams/syn3-normal-ready.bam $BASEDIR/final/syn3-normal/
+sed -i '' s%/Sid/vsaveliev/$BASEDIR/input/%../input/%g $BASEDIR/config/$BASEDIR.yaml
 cp bams/syn3-tumor-ready.bam $BASEDIR/final/syn3-tumor/
-rm $BASEDIR/final/syn3-normal/syn3-normal-ready.bam.bai
+cp bams/syn3-normal-ready.bam $BASEDIR/final/syn3-normal/
+cp bams/syn3-normal-germline-ready.bam $BASEDIR/final/syn3-normal-germline/
 rm $BASEDIR/final/syn3-tumor/syn3-tumor-ready.bam.bai
+rm $BASEDIR/final/syn3-normal/syn3-normal-ready.bam.bai
+rm $BASEDIR/final/syn3-normal-germline/syn3-normal-germline-ready.bam.bai
 mkdir $BASEDIR/input
 cp NGv3.chr21.4col.bed $BASEDIR/input
 
