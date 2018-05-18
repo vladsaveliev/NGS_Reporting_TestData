ebc735001da9fba4d77d2180e56cd0eca6005639
vladsaveliev <vladislav.sav@gmail.com>
2018-05-18 16:48:20 +1000

    Update test data. Skip germline samples

--- test_bcbio_postproc.py
+++ test_bcbio_postproc.py
@@ -6,30 +6,8 @@ from datetime import datetime
 import shutil
 import subprocess
 
-from ngs_utils.testing import BaseTestCase, info, check_call
+from ngs_utils.testing import BaseTestCase, info, check_call, html_wrapper, vcf_ignore_lines
 from ngs_utils.utils import is_az, is_local, is_travis
-
-
-REUSE = False      # Run on top of existing latest results
-ONLY_DIFF = False   # Do not run, just diff the latest results against the gold standard
-
-# Find and parse all elements containing json data, put data into a list and dumps the result.
-# The resulting text is unique per json data, so we can run simple `diff` on them.
-html_wrapper = [
-    'grep', '-A1', '<div id=".*_json">', '|', 'grep', '-v', '<div id=".*_json">', '|',
-    'python', '-c',
-        'import sys, json; '
-        'sys.stdout.write(json.dumps([json.loads(el) for el in sys.stdin.read().split(\'--\')], '
-                                     'indent=2, sort_keys=True))'
-]
-
-vcf_ignore_lines = [
-    'bcftools_annotateVersion',
-    'bcftools_annotateCommand',
-    '^##INFO=',
-    '^##FILTER=',
-    '^##contig=',
-]
 
 
 class Test_bcbio_postproc(BaseTestCase):
@@ -39,46 +17,38 @@ class Test_bcbio_postproc(BaseTestCase):
     results_dir = join(dirname(__file__), BaseTestCase.results_dir, script)
     gold_standard_dir = join(dirname(__file__), BaseTestCase.gold_standard_dir, script)
 
+    reuse = False  # Run on top of existing latest results. Also controlled with TEST_REUSE
+    only_diff = False  # Do not run, just diff the latest results against the gold standard. Also controlled with TEST_ONLY_DIFF
+
     def setUp(self):
-        if is_local():
-            info('Running locally: setting up PATH')
-            os.environ['PATH'] = '/Users/vlad/miniconda3/envs/ngs_reporting/bin:' + expanduser('~/bin') + ':/usr/local/bin:/usr/bin:/bin:/usr/sbin:' + os.environ['PATH']
-            info('PATH = ' + os.environ['PATH'])
+        # if is_local():
+        #     info('Running locally: setting up PATH')
+        #     os.environ['PATH'] = '/Users/vlad/miniconda3/envs/ngs_reporting/bin:' + expanduser('~/bin') + ':/usr/local/bin:/usr/bin:/bin:/usr/sbin:' + os.environ['PATH']
+        #     info('PATH = ' + os.environ['PATH'])
         BaseTestCase.setUp(self)
 
     def _run_postproc(self, bcbio_dirname, eval_panel=False, parallel=False, debug=True):
         results_dir = join(self.results_dir, bcbio_dirname)
+        bcbio_dir = join(self.data_dir, bcbio_dirname)
+
+        cmdl = [self.script, results_dir]
+        if eval_panel:
+            cmdl.append('--eval-panel')
+        if not parallel:
+            cmdl.extend(['-t', '1'])
+        if debug:
+            cmdl.append('-d')
+
         run_with_error = False
-
-        if not ONLY_DIFF:
-            bcbio_dir = join(self.data_dir, bcbio_dirname)
-            assert isdir(bcbio_dir), 'data dir ' + bcbio_dir + ' not found'
-
-            if not REUSE:
-                if exists(results_dir):
-                    last_changed = datetime.fromtimestamp(getctime(results_dir))
-                    prev_run = results_dir + '_' + last_changed.strftime('%Y_%m_%d_%H_%M_%S')
-                    os.rename(results_dir, prev_run)
-                shutil.copytree(bcbio_dir, results_dir, symlinks=True)
-
-            cmdl = [self.script, results_dir]
-            if eval_panel:
-                cmdl.append('--eval-panel')
-            if not parallel:
-                cmdl.extend(['-t', '1'])
-            if debug:
-                cmdl.append('-d')
-
-            run_with_error = False
-            info('-' * 100)
-            try:
-                check_call(cmdl)
-            except subprocess.CalledProcessError:
-                sys.stderr.write(self.script + ' finished with error:\n')
-                sys.stderr.write(traceback.format_exc() + '\n')
-                run_with_error = True
-            info('-' * 100)
-            info('')
+        info('-' * 100)
+        try:
+            self._run_cmd(cmdl, bcbio_dir, results_dir, before_run_fn=lambda: shutil.copytree(bcbio_dir, results_dir, symlinks=True))
+        except subprocess.CalledProcessError:
+            sys.stderr.write(self.script + ' finished with error:\n')
+            sys.stderr.write(traceback.format_exc() + '\n')
+            run_with_error = True
+        info('-' * 100)
+        info('')
 
         return results_dir, run_with_error
 
@@ -100,13 +70,13 @@ class Test_bcbio_postproc(BaseTestCase):
         failed = self._check_file(failed, join(datestamp_dir, 'var', caller + '.REJECT.txt'))
         failed = self._check_file(failed, join(datestamp_dir, 'var', caller + '.PASS.json'))
         if caller != 'freebayes':  # cannot merge using `bcftools merge`: > Incorrect number of AD fields (3) at chr21:11049225, cannot merge.
-            failed = self._check_file(failed, join(datestamp_dir, 'var', caller + '.vcf.gz'), vcf_ignore_lines)
+            failed = self._check_file(failed, join(datestamp_dir, 'var', caller + '.vcf.gz'), vcf_ignore_lines, check_diff=False)
         return failed
 
     def _check_var_in_sample(self, failed, sample_dir, sample, caller):
-        failed = self._check_file(failed, join(sample_dir, 'varAnnotate', sample + '-' + caller + '.anno.vcf.gz'), vcf_ignore_lines)
-        failed = self._check_file(failed, join(sample_dir, 'varFilter', sample + '-' + caller + '.anno.filt.vcf.gz'), vcf_ignore_lines)
-        failed = self._check_file(failed, join(sample_dir, 'varFilter', sample + '-' + caller + '.anno.filt.PASS.vcf.gz'), vcf_ignore_lines)
+        failed = self._check_file(failed, join(sample_dir, 'varAnnotate', sample + '-' + caller + '.anno.vcf.gz'), vcf_ignore_lines, check_diff=False)
+        failed = self._check_file(failed, join(sample_dir, 'varFilter', sample + '-' + caller + '.anno.filt.vcf.gz'), vcf_ignore_lines, check_diff=False)
+        failed = self._check_file(failed, join(sample_dir, 'varFilter', sample + '-' + caller + '.anno.filt.PASS.vcf.gz'), vcf_ignore_lines, check_diff=False)
         failed = self._check_file(failed, join(sample_dir, 'varFilter', caller + '.PASS.json'), check_diff=False)
         failed = self._check_file(failed, join(sample_dir, 'varFilter', caller + '.PASS.txt'))
         failed = self._check_file(failed, join(sample_dir, 'varFilter', caller + '.txt'))
@@ -127,8 +97,8 @@ class Test_bcbio_postproc(BaseTestCase):
         failed = self._check_file(failed, join(datestamp_dir, 'cnv', 'seq2c.tsv'), wrapper=['sort'])
         failed = self._check_file(failed, join(datestamp_dir, 'cnv', 'seq2c.filt.tsv'), wrapper=['sort'])
         failed = self._check_file(failed, join(datestamp_dir, 'cnv', 'seq2c-coverage.tsv'), wrapper=['sort'])
-        failed = self._check_file(failed, join(datestamp_dir, 'cnv', 'cnvkit.tsv'), wrapper=['sort'])
-        failed = self._check_file(failed, join(datestamp_dir, 'cnv', 'cnvkit.filt.tsv'), wrapper=['sort'])
+        # failed = self._check_file(failed, join(datestamp_dir, 'cnv', 'cnvkit.tsv'), wrapper=['sort'])
+        # failed = self._check_file(failed, join(datestamp_dir, 'cnv', 'cnvkit.filt.tsv'), wrapper=['sort'])
         for caller, samples in callers.items():
             if all(s.endswith('-germline') for s in samples):
                 failed = self._check_var_in_datestamp(failed, datestamp_dir, caller + '-germline')
@@ -155,7 +125,8 @@ class Test_bcbio_postproc(BaseTestCase):
 
     def test_03_paired_with_germline(self):
         self._test_dream_chr20('dream_chr21_paired_with_germline',
-                               callers={'vardict': ['syn3-tumor'], 'gatk-haplotype': ['syn3-normal-germline']})
+                               # callers={'vardict': ['syn3-tumor'], 'gatk-haplotype': ['syn3-normal']})
+                               callers={'vardict': ['syn3-tumor']})
 
     def test_04_rnaseq(self):
         bcbio_proj_dir, run_with_error = self._run_postproc(bcbio_dirname='rnaseq_hg38_hisat2')
